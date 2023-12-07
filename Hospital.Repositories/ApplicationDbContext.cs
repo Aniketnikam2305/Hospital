9cb3f655e8c214f9fbc013150ab8b9ca57fb146a
aniketnikam2305 <aniketnikam2305@gmail.com>
2023-12-07 09:38:48 +0530

    Start

--- Hospital.Repositories/ApplicationDbContext.cs
+++ Hospital.Repositories/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,11 @@ namespace Hospital.Repositories
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Console.WriteLine();
+        
+        
+        }
     }
 }
